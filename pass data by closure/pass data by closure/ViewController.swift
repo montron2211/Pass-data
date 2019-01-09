29fcbd95a37a42f151ff6aeffada607816754c0b
Mon <mon@Admins-iMac.local>
2019-01-09 09:44:48 +0700

    them project truyen du lieu qua closure

---
+++ pass data by closure/pass data by closure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pass data by closure
+//
+//  Created by Mon on 1/9/19.
+//  Copyright © 2019 Mon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ pass data by closure/pass data by closureTests/pass_data_by_closureTests.swift
@@ -0,0 +1,34 @@
+//
+//  pass_data_by_closureTests.swift
+//  pass data by closureTests
+//
+//  Created by Mon on 1/9/19.
+//  Copyright © 2019 Mon. All rights reserved.
+//
+
+import XCTest
+@testable import pass_data_by_closure
+
+class pass_data_by_closureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
